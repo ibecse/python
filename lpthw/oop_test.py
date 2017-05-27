2cddbad979136b78f83703ee7e9d0f85da08ec20
Becse <zerofly28@gmail.com>
2017-05-27 14:27:34 +0800

    Becse

--- lpthw/oop_test.py
+++ lpthw/oop_test.py
@@ -7,17 +7,17 @@ WORDS = []
 
 PHRASES = {
     "class %%%(%%%):":
-        "Make a calss named %%% that is-a %%%.",
+        "Make a class named %%% that is-a %%%.",
     "class %%%(object):\n\tdef __init__(self, ***)":
         "class %%% has-a __init__ that takes self and *** parameters.",
     "class %%%(object):\n\tdef ***(self, @@@)":
-        "class %%% has-a funtion named *** that takes self and @@@ parameters.",
+        "class %%% has-a function named *** that takes self and @@@ parameters.",
     "*** = %%%()":
         "Set *** to an instance of class %%%.",
     "***.***(@@@)":
         "From *** get the *** function, and call it with parameters self, @@@.",
     "***.*** = '***'":
-        "From *** get the *** atrribute and set it to '***'."
+        "From *** get the *** attribute and set it to '***'."
 }
 
 # do they want to drill phrases first
@@ -26,10 +26,14 @@ if len(sys.argv) == 2 and sys.argv[1] == "english":
 else:
     PHRASE_FIRST = False
 
-# load up the words frome the website
+# load up the words from the website
+# strip() 方法用于移除字符串头尾指定的字符（默认为空格）。str.strip([chars])
 for word in urlopen(WORD_URL).readlines():
     WORDS.append(word.strip())
 
+# capitalize()将字符串的第一个字母变成大写,其他字母变小写。str.capitalize()
+# random.sample()可以从指定的序列中，随机的截取指定长度的片断，不作原地修改。random.sample(str, num)
+# count() 方法用于统计字符串里某个字符出现的次数。可选参数为在字符串搜索的开始与结束位置。str.count(sub, start= 0, end=len(string))
 def convert(snippet, phrase):
     class_names = [w.capitalize() for w in
                    random.sample(WORDS, snippet.count("%%%"))]
@@ -37,10 +41,12 @@ def convert(snippet, phrase):
     results = []
     param_names = []
 
+# random.randint()生成一个指定范围的随机整数。random.randint(a,b)
     for i in range(0, snippet.count("@@@")):
         param_count = random.randint(1, 3)
         param_names.append(', '.join(random.sample(WORDS, param_count)))
 
+# replace()方法把字符串中的old（旧字符串）替换成new(新字符串)，如果指定第三个参数max，则替换不超过max次。str.replace(old, new[, max])
     for sentence in snippet, phrase:
         result = sentence[:]
         # fake class names
@@ -60,6 +66,7 @@ def convert(snippet, phrase):
     return results
 
 # keep going until the hit CTRL-D
+# random.shuffle() 方法将序列的所有元素随机排序。random.shuffle(lst)
 try:
     while True:
         snippets = PHRASES.keys()
