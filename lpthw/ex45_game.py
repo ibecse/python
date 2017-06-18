110919efb80023fb9428eab5635af3305baec612
Becse <zerofly28@gmail.com>
2017-06-18 21:54:28 +0800

    lpthw

---
+++ lpthw/ex45_game.py
@@ -0,0 +1,45 @@
+class Address(object):
+    pass
+
+class Transportation(object):
+    pass
+
+class Time(object):
+    pass
+
+class Fail(object):
+    pass
+
+class Win(object):
+    pass
+
+class Home(Address):
+    pass
+
+class BusStation(Address):
+    pass
+
+class SubwayStation(Address):
+    pass
+
+class CBD(Address):
+    pass
+
+class Bike(Transportation):
+    pass
+
+class Bus(Transportation):
+    pass
+
+class Subway(Transportation):
+    pass
+
+class SevenAm(Time):
+    pass
+
+class NineAm(Time):
+    pass
+
+class SixPm(Time):
+    pass
+
