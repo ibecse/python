4659ce08c045c73e4667e4c406548288f92d1fd6
ibecse <zerofly28@gmail.com>
2019-05-07 14:21:38 +0800

    becse

---
+++ BIT-268001/TempConvert.py
@@ -0,0 +1,10 @@
+#TempConvert
+TempStr = input("请输入需要转换的温度：")
+if TempStr[-1] in ['c','C']:
+    F = 1.8*eval(TempStr[0:-1]) + 32
+    print("华氏温度为：{:.2f}F".format(F))
+elif TempStr[-1] in ['f','F']:
+    C = (eval(TempStr[0:-1])-32)/1.8
+    print("摄氏温度为：{:.2f}C".format(C))
+else:
+    print("输入错误！")
