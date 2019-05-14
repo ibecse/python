f6ea2ab84313836fa541689650b735ff11282010
ibecse <zerofly28@gmail.com>
2019-05-14 22:05:47 +0800

    Becse

---
+++ BIT-268001/WeekNamePrintV1.py
@@ -0,0 +1,5 @@
+# WeekNamePrintV1.py
+WeekStr = "星期一星期二星期三星期四星期五星期六星期日"
+WeekId = eval(input("请输入星期数字1-7："))
+pos = (WeekId - 1) * 3
+print(WeekStr[pos:pos+3])
---
+++ BIT-268001/WeekNamePrintV2.py
@@ -0,0 +1,4 @@
+# WeekNamePrintV2.py
+WeekStr = "一二三四五六日"
+WeekId = eval(input("请输入星期数字1-7："))
+print("星期" + WeekStr[WeekId-1])
