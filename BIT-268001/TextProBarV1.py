ab9e7be543836f5c229b033cad5c1d0a16f856e7
ibecse <zerofly28@gmail.com>
2019-05-15 22:03:33 +0800

    Becse

---
+++ BIT-268001/TextProBarV1.py
@@ -0,0 +1,11 @@
+# TextProBarV1.py
+import time
+scale = 10
+print("-----执行开始-----")
+for i in range(scale + 1):
+    a = (i/scale) * 100
+    b = i * '*'
+    c = (scale-i) * '.'
+    print("{:^3.0f}%[{}->{}]".format(a, b, c))
+    time.sleep(1)
+print("-----执行结束-----")
---
+++ BIT-268001/TextProBarV3.py
@@ -0,0 +1,13 @@
+# TextProBarV3.py
+import time
+scale = 100
+print("执行开始".center(scale // 2, '-'))
+start = time.perf_counter()
+for i in range(scale + 1):
+    a = (i/scale) * 100
+    b = i * '*'
+    c = (scale-i) * '.'
+    dur = time.perf_counter() - start
+    print("\r{:^3.0f}%[{}->{}]{:.2f}s".format(a, b, c, dur), end='')
+    time.sleep(0.1)
+print("\n" + "执行结束".center(scale // 2, '-'))
