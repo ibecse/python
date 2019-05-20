3bd9eb5d2028095f97bec2ef56c58ede4143dd07
ibecse <zerofly28@gmail.com>
2019-05-20 09:40:35 +0800

    Becse

--- BIT-268001/Untitled-1.py
+++ BIT-268001/Untitled-1.py
@@ -1,11 +1,18 @@
-Num = input("请输入一个整数：")
-Num = eval(Num)
-Str = 'Hello World'
-if Num == 0:
-    print("Hello World")
-elif Num > 0:
-    for i in range(2, len(Str)):
-        print(Str[i-2:i])
-else:
-    for i in range(len(Str)):
-        print(Str[i])
+Str = input()
+for i in range(len(Str)):
+    if i == '+':
+        M = eval(Str[:Str.find('+')])
+        N = eval(Str[Str.find('+') + 1:])
+        print("{:.2f}".format(M + N))
+    if i == '-':
+        M = eval(Str[:Str.find('-')])
+        N = eval(Str[Str.find('-') + 1:])
+        print("{:.2f}".format(M - N))
+    if i == '*':
+        M = eval(Str[:Str.find('*')])
+        N = eval(Str[Str.find('*') + 1:])
+        print("{:.2f}".format(M * N))
+    if i == '/':
+        M = eval(Str[:Str.find('/')])
+        N = eval(Str[Str.find('/') + 1:])
+        print("{:.2f}".format(M / N))
