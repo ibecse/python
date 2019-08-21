a1a3aba6dfacf3b48f6d4b9a9f45d723393c06df
ibecse <zerofly28@gmail.com>
2019-08-22 00:29:04 +0800

    Update para.py

--- para.py
+++ para.py
@@ -3,7 +3,7 @@ import time
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname="192.168.199.1", port=22, username="becse", password="18978121477", timeout=10)
+ssh.connect(hostname="", port=22, username="", password="", timeout=10)
 ssh_shell = ssh.invoke_shell()
 cmd = ['configure', 'set interfaces ethernet eth2 address 1.1.1.2/24', 'commit', 'save']
 
